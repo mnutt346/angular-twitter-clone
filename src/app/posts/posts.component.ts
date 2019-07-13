14322b77b3efd1cf5b1846d24265f423a456bf2f
Michael Nutt <mnutt346@gmail.com>
2019-07-13 17:52:31 -0500

    Bug fix: Randomly generated tweets render to DOM.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Posts } from "../tweet-generator";
 import { Post } from "../post";
 import { PostsService } from "../posts.service";
 
@@ -12,16 +11,106 @@ export class PostsComponent implements OnInit {
   constructor(private postsService: PostsService) {}
 
   posts: Post[];
+  originalPosts = [this.posts];
+  filtered = false;
 
   getPosts(): void {
     this.postsService.getPosts().subscribe(posts => (this.posts = posts));
   }
 
-  public onSelect(e): void {
+  onSelect(e): void {
     this.posts = this.posts.filter(post => post.user === e.target.id);
   }
 
   ngOnInit() {
     this.getPosts();
+    this.tenSeconds();
   }
+
+  usernames = [
+    "BobLobLaw",
+    "WhoWhatWhenWhereAndWhy",
+    "User13135",
+    "Noob",
+    "ABCDEFB"
+  ];
+
+  beginning = [
+    "just",
+    "ask me how i",
+    "totally",
+    "nearly",
+    "productively",
+    "Who",
+    "last night i",
+    "the president",
+    "that dude",
+    "a dinosaur",
+    "a dog"
+  ];
+
+  verbs = [
+    "jumpped",
+    "ran",
+    "fell",
+    "drank",
+    "slipped",
+    "sat",
+    "saw",
+    "went",
+    "tripped",
+    "sped",
+    "built",
+    "started"
+  ];
+
+  posess = ["your", "the", "my", "that", "this", "a", "an"];
+
+  nouns = [
+    "car",
+    "cat",
+    "bike",
+    "dog",
+    "house",
+    "mouse",
+    "tree",
+    "mountain",
+    "building",
+    "computer",
+    "Nicolas Caige"
+  ];
+
+  selectRandomElement = arr => {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  };
+
+  createRandomSentence = () => {
+    return `${this.selectRandomElement(
+      this.beginning
+    )} ${this.selectRandomElement(this.verbs)} ${this.selectRandomElement(
+      this.posess
+    )} ${this.selectRandomElement(this.nouns)}`;
+  };
+
+  createPost = () => {
+    let post = new Post();
+    post.user = this.selectRandomElement(this.usernames);
+    post.content = this.createRandomSentence();
+    post.timeStamp = new Date();
+    return post;
+  };
+
+  tenTweets = () => {
+    for (let i = 0; i < 10; i++) {
+      let newPost = this.createPost();
+      this.posts.push(newPost);
+      console.log(this.posts);
+    }
+  };
+
+  tenSeconds = () => {
+    this.tenTweets();
+    setTimeout(this.tenSeconds, 10000);
+  };
 }
